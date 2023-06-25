1cb6d5b25716fb25186b98a9540955eacf2809c7
Ujjwal <ujjwalchoudhary065@gmail.com>
2023-06-25 20:41:25 +0530

    Added Authentication

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,90 @@
+'use client'
+import { cn } from '@/lib/utils'
+import { FC, useState } from 'react'
+import { Button } from './ui/Button'
+import {signIn} from 'next-auth/react';
+import { Icons } from './Icons';
+import { useToast } from '@/hooks/use-toast';
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement>{}
+
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props}) => {
+   
+    const {toast} = useToast()
+   const [isLoading, setIsLoading] = useState<boolean>(false)
+   
+
+   const loginWithGoogle = async() =>{
+    setIsLoading(true)
+    try{
+        await signIn('google')
+
+    }catch (error){
+      toast({
+        title: 'There was a problem.',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive',
+      })
+
+    } finally{
+        setIsLoading(false)
+    }
+   }
+
+
+   const loginWithTwitter = async() =>{
+    setIsLoading(true)
+    try{
+        await signIn('twitter')
+
+    }catch (error){
+        //toast notification
+        toast({
+            title: 'There was a problem.',
+            description: 'There was an error logging in with Twitter',
+            variant: 'destructive',
+          })
+
+    } finally{
+        setIsLoading(false)
+    }
+   }
+
+   const loginWithGithub = async() =>{
+    setIsLoading(true)
+    try{
+        await signIn('github')
+
+    }catch (error){
+        //toast notification
+        toast({
+            title: 'There was a problem.',
+            description: 'There was an error logging in with Github',
+            variant: 'destructive',
+          })
+
+    } finally{
+        setIsLoading(false)
+    }
+   }
+
+  return (
+  <div className={cn('flex flex-col gap-2 justify-center', className)}{...props}>
+    <Button onClick={loginWithGoogle} isLoading={isLoading} size='sm' className='w-full'
+    
+    > {isLoading ? null : <Icons.google className='h-4 w-4 mr-2 ' />} Google</Button>
+
+
+   <Button onClick={loginWithTwitter} isLoading={isLoading} size='sm' className='w-full gap-2'
+    
+    > {isLoading ? null : <Icons.TwitterIcon className='h-4 w-4 ' />} Twitter</Button>
+
+   <Button onClick={loginWithGithub} isLoading={isLoading} size='sm' className='w-full gap-2'
+    
+    > {isLoading ? null : <Icons.GitHubIcon className='h-4 w-4 ' />} Github</Button>
+  </div>
+  )
+}
+
+export default UserAuthForm
---
+++ src/lib/auth.ts
@@ -0,0 +1,79 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { db } from "./db";
+import { nanoid } from 'nanoid'
+import GoogleProvider from 'next-auth/providers/google'
+import TwitterProvider from 'next-auth/providers/twitter'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session:{
+        strategy: 'jwt',
+
+    },
+    pages: {
+        signIn: '/sign-in',
+    },
+    providers : [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        TwitterProvider({
+            clientId: process.env.TWITTER_CLIENT_ID!,
+            clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        })
+    ],
+
+    callbacks: {
+        async session({ token, session }){
+            if(token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+                session.user.username = token.username
+    
+            }
+            return session
+        },
+        async jwt({ token, user }) {
+            const dbUser = await db.user.findFirst({
+                where:{
+                    email: token.email,
+                },
+            })
+
+            if(!dbUser){
+                token.id = user!.id
+                return token
+            }
+
+            if(!dbUser.username) {
+                await db.user.update({
+                    where:{
+                        id: dbUser.id,
+                    },
+                    data: {
+                        username: nanoid(10),
+                    }
+                })
+            }
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image,
+                username: dbUser.username,
+            }
+        },
+        redirect(){
+            return '/'
+        }
+    },
+}
